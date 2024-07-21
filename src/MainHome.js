343e7a5a9c9d1a5fde6a6b5efb7a894041139f75
rladmstj <pingdoll3110@naver.com>
2024-07-21 18:13:41 +0900

    login design

--- src/MainHome.js
+++ src/MainHome.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './MainHome.css';
 const MainHome = () => {
   return (
     <div className="mainhome">
       <h1>Welcome to Pixel Runner</h1>
+      <div className="character"></div> {/* 캐릭터 애니메이션 추가 */}
       <div className="menu">
         <Link to="/tutorial">
           <button>Tutorial</button>
